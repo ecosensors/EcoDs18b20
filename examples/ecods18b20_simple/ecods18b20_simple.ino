e2b3c826e9b2e6e9e3c2313b4706de7594f3a9e0
EcoSensors <65593795+ecosensors@users.noreply.github.com>
2022-06-01 16:21:53 +0200

    Update ecods18b20_simple.ino

--- examples/ecods18b20_simple/ecods18b20_simple.ino
+++ examples/ecods18b20_simple/ecods18b20_simple.ino
@@ -1,36 +1,40 @@
 #include <OneWire.h>
 #include <Ecods18b20.h>
 
-OneWire ds(19); // read the sensors at the pin19 (A1)
+OneWire ds(19);               // read the sensors at the pin19 (A1)
 byte pinPower=10;
 byte pinRead=19; //A1
+
+float temperature_soil;
 
 Ecods18b20 ecods18b20;
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(9600);
-  ecods18b20.start(&ds);
   pinMode(pinPower, OUTPUT);
   digitalWrite(pinPower,LOW);
   pinMode(pinRead, INPUT_PULLUP);
 
-  float temperature_soil;
+  ecods18b20.start(&ds);
+  
 
-  // Test it
+  /* Test it */
   // Power the sensor
+  
   //expander.digitalWrite(station.tsoil_config.pinPower,HIGH)
   digitalWrite(pinPower,HIGH);
+
   if(ecods18b20.get_temperature(&temperature_soil, true)==1)
   {
     Serial.print((int16_t)temperature_soil); Serial.println(F("C"));
   }
+  
   digitalWrite(pinPower,LOW);
   //expander.digitalWrite(station.tsoil_config.pinPower,HIGH)
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
 }
